3a85412e6d6e6d2410852684169f5f7f51cda900
Sharang Phadke <sharang.phadke@gmail.com>
2022-07-27 16:17:15 -0400

    cognito auth

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_6ZLl7vNjR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1gbsi4qr1nfa01fp1qvsbi83cg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
